736d23f9eab9f6f5e9001835695d39c636473520
Juancamontero <juancamilomonteroosorio@192.168.1.104>
2024-06-11 16:31:48 -0500

    lesson ended

--- src/script.js
+++ src/script.js
@@ -8,6 +8,9 @@ import GUI from 'lil-gui'
  */
 // Debug
 const gui = new GUI()
+const debugObject = {
+  rotationSpeed: 0.4,
+}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -25,30 +28,30 @@ const cubeTextureLoader = new THREE.CubeTextureLoader()
 /**
  * Update all materials
  */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            child.material.envMapIntensity = 1
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      child.material.envMapIntensity = 1
+      child.material.needsUpdate = true
+      child.castShadow = true
+      child.receiveShadow = true
+    }
+  })
 }
 
 /**
  * Environment map
  */
 const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
 ])
 
 scene.background = environmentMap
@@ -64,28 +67,143 @@ mapTexture.colorSpace = THREE.SRGBColorSpace
 const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
 
 // Material
-const material = new THREE.MeshStandardMaterial( {
-    map: mapTexture,
-    normalMap: normalTexture
+const material = new THREE.MeshStandardMaterial({
+  map: mapTexture,
+  normalMap: normalTexture,
 })
+
+// ? we need to create a depthMaterial to change it with the shadows
+// ? we will override the existen one in the standardMaterial
+// todo: Add to the model
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking, //! to load en every RGB channel
+})
+
+//********* Hooking the material ***********//
+//********* -------------------- ***********//
+// ? 'node_modules/three/src/renderers/shaders/ShaderLib/meshphysical.glsl.js'
+
+// * create an object so i can map later the uniforms inside the beforeCompile
+const customUniforms = {
+  uTime: {
+    value: 0,
+  },
+}
+
+material.onBeforeCompile = (shader) => {
+  // * Add uniforms
+  shader.uniforms.uTime = customUniforms.uTime
+
+  // * out main()
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+    #include <common>
+    uniform float uTime;
+
+    mat2 get2dRotateMatrix(float _angle)
+    {
+      return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+    }   
+    `
+  )
+
+  // ****** UPDATE CORE SHADOWS ********//
+// ? Own shadows
+// ? this about the normal, which must be updated to
+shader.vertexShader = shader.vertexShader.replace(
+  '#include <beginnormal_vertex>',
+  `
+  #include <beginnormal_vertex>
+
+    float angle = sin(position.y + uTime) * ${debugObject.rotationSpeed};
+    mat2 rotateMatrix = get2dRotateMatrix(angle);
+    objectNormal.xz = rotateMatrix * objectNormal.xz;
+  
+  `
+)
+
+  // * inside main()
+  // ? transformed in the matrix that can be changed generated in
+  // ? node_modules/three/src/renderers/shaders/ShaderChunk/begin_vertex.glsl.js
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+    #include <begin_vertex>  
+    
+    // !this 2 already in the core shadow adjustment
+    // !float angle = (position.y + uTime) * 0.9;
+    //! mat2 rotateMatrix = get2dRotateMatrix(angle);
+    transformed.xz = rotateMatrix * transformed.xz;
+    `
+  )
+}
+
+// ****** UPDATE DROP SHADOWS ********//
+// ? Reflected shadows
+depthMaterial.onBeforeCompile = (shader) => {
+  // * Add uniforms
+  shader.uniforms.uTime = customUniforms.uTime
+
+   // * out main()
+   shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+    #include <common>
+
+    uniform float uTime;
+
+    mat2 get2dRotateMatrix(float _angle)
+    {
+      return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+    }
+    `
+  )
+
+  // * inside main()
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+    #include <begin_vertex>  
+    
+    float angle = sin(position.y + uTime) * ${debugObject.rotationSpeed};
+    mat2 rotateMatrix = get2dRotateMatrix(angle);
+    transformed.xz = rotateMatrix * transformed.xz;
+    `
+  )
+}
+
+
+
+
 
 /**
  * Models
  */
-gltfLoader.load(
-    '/models/LeePerrySmith/LeePerrySmith.glb',
-    (gltf) =>
-    {
-        // Model
-        const mesh = gltf.scene.children[0]
-        mesh.rotation.y = Math.PI * 0.5
-        mesh.material = material
-        scene.add(mesh)
+gltfLoader.load('/models/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
+  // Model
+  const mesh = gltf.scene.children[0]
+  mesh.rotation.y = Math.PI * 0.5
+  mesh.material = material
+  mesh.customDepthMaterial = depthMaterial // ? to adjust shadows
+  scene.add(mesh)
 
-        // Update materials
-        updateAllMaterials()
-    }
+  // Update materials
+  updateAllMaterials()
+})
+
+//**
+// ** PLANE
+// */
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(15, 15, 15),
+  new THREE.MeshStandardMaterial({})
 )
+plane.rotation.y = Math.PI
+plane.position.y = -5
+plane.position.z = 5
+scene.add(plane)
 
 /**
  * Lights
@@ -95,38 +213,42 @@ directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 2, - 2.25)
+directionalLight.position.set(0.25, 2, -2.25)
 scene.add(directionalLight)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.set(4, 1, -4)
 scene.add(camera)
 
 // Controls
@@ -137,8 +259,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true,
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFShadowMap
@@ -152,18 +274,20 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update objects
+  customUniforms.uTime.value = elapsedTime
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
